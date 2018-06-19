6930c15f27840b4f0f0a9ffd7ad2b283be1b8790
Nick Yegorov <nick.yegorov@gmail.com>
2018-06-19 15:06:03 +0300

    use std::nullopt

--- monads.h
+++ monads.h
@@ -3,21 +3,21 @@
 namespace monads {
 
 // generic monad interface
-template<class A, class F> constexpr auto mmap(A&& a, F f)		{ return f(std::forward<A>(a)); };
-template<class ...A, class F> constexpr auto mmap(const std::tuple<A...>& t, F f) { return std::apply(f, t); };
-template<class ...A, class F> constexpr auto mmap(std::tuple<A...>&& t, F f) { return std::apply(f, std::move(t)); };
-template<class A> constexpr auto mjoin(A&& a)					{ return std::forward<A>(a); };
-template<class MA, class F> constexpr auto mapply(MA&& ma, F f)	{ return mjoin(mmap(ma, f)); }
+template<class A, class F> constexpr auto mmap(A&& a, F f)						{ return f(std::forward<A>(a)); };
+template<class...A, class F> constexpr auto mmap(const std::tuple<A...>& t, F f){ return std::apply(f, t); };
+template<class...A, class F> constexpr auto mmap(std::tuple<A...>&& t, F f)		{ return std::apply(f, std::move(t)); };
+template<class A> constexpr auto mjoin(A&& a)									{ return std::forward<A>(a); };
+template<class MA, class F> constexpr auto mapply(MA&& ma, F f)					{ return mjoin(mmap(ma, f)); }
 
 // monad binding operator
-template<typename F> struct fwrap { F f; };
+template<typename F> struct fwrap {const F f; };
 template<class MA, typename F> constexpr auto operator | (MA&& ma, F f)			{ return mapply(std::forward<MA>(ma), f); }
 template<class MA, typename F> constexpr auto operator | (MA&& ma, fwrap<F>& f)	{ return f.f(std::forward<MA>(ma)); };
 template<class MA, typename F> constexpr auto operator | (MA&& ma, fwrap<F>&& f){ return f.f(std::forward<MA>(ma)); };
 template<class F> constexpr auto operator ~ (F f)								{ return fwrap<F> {f}; };
 
 template <class C, class B> struct rebind;
-template <template<class...> class C, class A, class B> struct rebind<C<A>, B> { typedef C<B> type; };
+template <template<class...> class C, class A, class B> struct rebind<C<A>, B>	{ typedef C<B> type; };
 template <template<class...> class C, class A, template<class> class AL, class B> struct rebind<C<A,AL<A>>, B> { typedef C<B,AL<B>> type; };
 
 // special aggregate functions 
@@ -32,7 +32,7 @@ template<typename F> struct filter_t
 	template<typename A> constexpr auto operator()(std::experimental::generator<A> ga) const {
 		for(auto&& a : ga)	if(_f(a)) co_yield a;
 	}
-	const F _f;
+	F _f;
 };
 template<typename F> constexpr auto filter(F f) { return ~filter_t<F>(f); }
 
@@ -58,12 +58,8 @@ using std::experimental::generator;
 using std::future;
 
 // optional
-template <class A, class F> constexpr auto mmap(optional<A>& o, F&& f) {
-	using OB = optional<decltype(mapply(std::declval<A>(), f))>;
-	return o ? OB{mapply(o.value(), f)} : OB{};
-};
-
-template <class A> constexpr auto mjoin(optional<optional<A>>&& o) {	return o ? o.value() : optional<A>{}; };
+template <class A, class F> constexpr auto mmap(optional<A>& o, F&& f)	{ return o ? optional{mapply(*o, std::forward<F>(f))} : std::nullopt; };
+template <class A> constexpr auto mjoin(optional<optional<A>>&& o)		{ return o ? *o : std::nullopt; };
 
 // list
 template <class A, template<class> class AL, class F> constexpr auto mmap(list<A, AL<A>>& la, F f) {
@@ -81,8 +77,8 @@ template <class A, template<class> class AL, class F> constexpr auto mmap(list<A
 };*/
 
 // generator
-template <class A, class F> constexpr auto mmap(generator<A>& ga, F f) {
-	for(auto&& a : ga) co_yield mapply(a, f);
+template <class A, class F> constexpr auto mmap(generator<A>& ga, F f) { 
+	for(auto&& a : ga) co_yield mapply(a, f); 
 };
 
 template <class A> constexpr auto mjoin(generator<generator<A>> gga) {
